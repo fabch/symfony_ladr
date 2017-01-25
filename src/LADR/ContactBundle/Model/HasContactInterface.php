ac1eccd34fe89f52544b60e1607afcf246faa18d
Fabien Chartrain <fabien.chartrain@gmail.com>
2017-01-25 15:10:11 +0100

    Création d'interface pour les entités qui possèdent 1 ou plusieurs contacts

---
+++ src/LADR/ContactBundle/Model/HasContactInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace LADR\ContactBundle\Model;
+
+use AppBundle\Entity\Contact;
+
+interface HasContactInterface
+{
+    /**
+     * Get contact
+     *
+     * @return Contact
+     */
+    public function getContact();
+
+    /**
+     * Set contact
+     *
+     * @param Contact $contact
+     * @return self
+     */
+    public function setContact($contact);
+
+}
