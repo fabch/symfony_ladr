f29adc80787edc2848eae1a4980226cde1dc54f9
Fabien Chartrain <fabien.chartrain@gmail.com>
2017-04-13 15:48:29 +0200

    modification orga templates/controller

--- src/LADR/ContactBundle/Controller/ContactController.php
+++ src/LADR/ContactBundle/Controller/ContactController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace LADR\ContactBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
